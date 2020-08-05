f3c46e7c1cf56d38d2208f2e2372f18e06223c1c
AratioD <fffb@ddd.com>
2020-08-05 22:19:40 +0300

    a basic validation is in place

--- descriptor.py
+++ descriptor.py
@@ -8,5 +8,42 @@ class Pricing:
 class Car:
     pass
 
+class ValidString:
+    def __init__(self, min_lenght=None):
+        self.min_lenght = min_lenght
+
+    def __set_name__(self, owner_class, property_name):
+        self.property_name = property_name
+
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise ValueError(f'{self.property_name} must be a string')
+        if self.min_lenght is not None and len(value) < self.min_lenght:
+            raise ValueError(
+                f'{self.property_name} must be at least {self.min_lenght} characters.'
+            )
+        instance.__dict__[self.property_name] = value
+
+    def __get__(self, instance, owner_class):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.property_name, None)
+    
+
 class Owner:
-    pass
+    first_name = ValidString(3)
+    last_name = ValidString(3)
+    
+
+
+p = Owner()
+
+
+try:
+    p.first_name = "Jack"
+    p.last_name = "Bauer"
+except ValueError as ex:
+    print(ex)
+    
+    
+print(p.first_name, p.last_name)
